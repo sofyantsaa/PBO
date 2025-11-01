41d00bd988716d59e07831a0dc5d75bd50ced1e3
sofyantsaa <sofyantsaori1464@gmail.com>
2025-11-01 22:50:07 +0700

    pertemuan3

---
+++ luaslingkaran.php
@@ -0,0 +1,15 @@
+<?php
+
+class LuasLingkaran {
+
+
+
+    public const phi = 3.14;
+    public int $jari;
+}
+$Lingkaran = new LuasLingkaran();
+$Lingkaran->jari = 7;
+
+$rumus = LuasLingkaran:: phi * $Lingkaran->jari * $Lingkaran->jari ;
+
+echo "Hasilnya Adalah :" . $rumus;
